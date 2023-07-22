d9da6f849bbc2dcde6d52d6d4e77d888f8246a68
SIMON WACHIRA <simonwachira7318@gmail.com>
2023-07-22 16:05:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// redirection of social media accounts
+function redirectToFacebook() {
+    window.location.href = "https://www.facebook.com/profile.php?id=100071584461977";
+  }
+  // redirection of social media accounts
+function redirectToInstagram() {
+    window.location.href = "https://instagram.com/its__wachira?igshid=NGExMmI2YTkyZg==";
+  }
+  
+  // redirection of social media accounts
+  function redirectToTwitter() {
+    window.location.href = "https://twitter.com/Its_wachira2?t=P5t0BhEmEFgfYGYECE7OdQ&s=35";
+  }
+  
+  // redirection of social media accounts
+  function redirectToLinkedin() {
+    window.location.href = "https://www.linkedin.com/in/simon-wachira-680b88268";
+  }
+  
+  // Toggle menu functionality
+  function toggleMenu() {
+    const navbarLinks = document.querySelector('.navbar-links');
+    navbarLinks.classList.toggle('active');
+  }
+  
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+
+// const navbarLinkks = document.getElementsByClassName('navbar-links')[0]
+
+// toggleButton.addEventListener('click', () => {
+//   navbarLinkks.classList.toggle('active')
+// })
